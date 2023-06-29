21a923f9aa10d5acb11fd2c3bf29bdba84a3f194
Matheus <matheustdl02@gmail.com>
2023-06-29 15:58:56 -0300

    feat: model finalizado

--- copies/models.py
+++ copies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Copy(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    estado_choices = [
+        ("disponivel", "Disponível"),
+        ("emprestada", "Emprestada"),
+        ("danificada", "Danificada"),
+        ("perdida", "Perdida"),
+    ]
+    estado = models.CharField(max_length=20, choices=estado_choices)
+    livro = models.ForeignKey("books.Book", on_delete=models.CASCADE)
--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Loan(models.Model):
+    data_emprestimo = models.DateTimeField(auto_now_add=True)
+    data_devolucao = models.DateTimeField()
+    copia = models.ForeignKey("copies.Copy", on_delete=models.CASCADE)
+    usuario = models.ForeignKey("users.User", on_delete=models.CASCADE)
