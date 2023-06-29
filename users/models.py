ee796df93adb39eca915e1f54fd2bc795739c545
Lucas Ribeiro <103015314+LuckDev22@users.noreply.github.com>
2023-06-29 15:38:34 -0300

    feat: create model books

--- users/models.py
+++ users/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class GenderChoices(models.TextChoices):
+class CategoryChoices(models.TextChoices):
     TERROR = "Terror"
     COMEDIA = "Comedia"
     SUSPENSE = "Suspense"
     ROMANCE = "Romance"
     FANTASIA = "Fantasia"
+    OUTROS = "Outros"
 
 
 class User(AbstractUser):
@@ -16,5 +17,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     date_birth = models.DateField()
     cpf = models.IntegerField(max_length=11, unique=True)
-    gender_preference = models.CharField(choices=GenderChoices.choices)
+    category_preference = models.CharField(
+        choices=CategoryChoices.choices, default=CategoryChoices.choices.OUTROS
+    )
     is_student = models.BooleanField(null=True, default=False)
