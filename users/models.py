e12a9acfab21e933cbcda6292de5ca3645917938
Lucas Ribeiro <103015314+LuckDev22@users.noreply.github.com>
2023-06-29 14:54:15 -0300

    create model
    feat: create model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    # tipo = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
+    # historico_emprestimos = models.ManyToManyField(Emprestimo)
