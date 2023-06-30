ef2cf8afb9fe595e0cbc1c4741412cd891264a7c
Matheus <matheustdl02@gmail.com>
2023-06-30 16:00:30 -0300

    permission/view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .permission import UserPermission
+from django.contrib.auth.hashers import make_password
+
 
 # Create your views here.
+class UserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        return super().post(request, *args, **kwargs)
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_update(self, serializer):
+        password = self.request.data.get("password")
+        if password:
+            serializer.save(password=make_password(password))
+        else:
+            serializer.save()
