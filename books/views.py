e8e07187f34cdebfa8fdeea876ed0448639bc3b1
Lucas Ribeiro <103015314+LuckDev22@users.noreply.github.com>
2023-06-29 23:39:55 -0300

    feat: add books

--- books/views.py
+++ books/views.py
@@ -11,6 +11,3 @@ class BooksView(ListCreateAPIView):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
