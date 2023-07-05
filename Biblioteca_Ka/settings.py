8d88cfcc6977bd77b613abc738ade2464061bda0
aparnaaravindp <114520875+aparnaaravindp@users.noreply.github.com>
2023-07-05 10:50:33 -0300

    feat: add changes

--- Biblioteca_Ka/settings.py
+++ Biblioteca_Ka/settings.py
@@ -56,7 +56,7 @@ DJANGO_APPS = [
 ]
 
 INSTALLED_APPS = DJANGO_APPS + MY_APPS + THIRD_PARTY_APPS
-]
+
 
 THIRD_PARTY_APPS = ["rest_framework"]
 
@@ -111,12 +111,6 @@ DATABASES = {
     "test": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-=======
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_DB_HOST"),
-        "PORT": os.getenv("POSTGRES_DB_PORT"),
 
     }
 }
