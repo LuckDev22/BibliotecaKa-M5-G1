a3d65b133e9e660b1bd30182da4e72b64d50c57e
aparnaaravindp <114520875+aparnaaravindp@users.noreply.github.com>
2023-07-05 16:02:55 -0300

    feat:coipes url

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .views import BooksView
 
+from copies import views as copy_views
+
 urlpatterns = [
     path("books/", BooksView.as_view()),
+    path("books/<int:pk>/copies/", copy_views.CopyView.as_view())
 ]
